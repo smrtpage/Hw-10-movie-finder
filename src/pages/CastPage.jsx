5baee2bf2cb7f4c6c218579ab847eac81cfa32ce
Igor-Lysak <lisakigor485@gmail.com>
2023-10-10 19:40:04 +0300

    update

--- src/pages/CastPage.jsx
+++ src/pages/CastPage.jsx
@@ -19,16 +19,20 @@ function CastPage() {
     <div>
       {isLoading && <p>Loading...</p>}
       <h2>Cast: </h2>
-      <ul className="profile_list">
-        {credits.map((credit) => (
-          <li className="profile_card" key={credit.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200/${credit.profile_path}`}
-            />
-            {credit.name} - {credit.character}
-          </li>
-        ))}
-      </ul>
+      {credits.length == 0 ? (
+        <p>Movie doesnt have credits</p>
+      ) : (
+        <ul className="profile_list">
+          {credits.map((credit) => (
+            <li className="profile_card" key={credit.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200/${credit.profile_path}`}
+              />
+              {credit.name} - {credit.character}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -19,15 +19,18 @@ function ReviewsPage() {
     <div>
       {isLoading && <p>Loading...</p>}
       <h2>Reviews: </h2>
-      {reviews.length == 0 && <p>Movie doesnt have reviews</p>}
-      <ul>
-        {reviews.map((review) => (
-          <li key={review.id}>
-            <h4>{review.author}</h4>
-            <p>{review.content}</p>
-          </li>
-        ))}
-      </ul>
+      {reviews.length == 0 ? (
+        <p>Movie doesnt have reviews</p>
+      ) : (
+        <ul>
+          {reviews.map((review) => (
+            <li key={review.id}>
+              <h4>{review.author}</h4>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
